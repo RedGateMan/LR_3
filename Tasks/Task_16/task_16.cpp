134f8223c30cd17d9c07f78bbe3cd5c3364fe40a
RedGateMan <egor.chijov@gmail.com>
2021-09-15 09:46:23 +0300

    https://github.com/RedGateMan/LR_2

--- Tasks/Task_16/task_16.cpp
+++ Tasks/Task_16/task_16.cpp
@@ -8,5 +8,20 @@
 using namespace std;
 
 int task_16(){
+    long long firstNumber, secondNumber, temp1, temp2;
+    cout << "Enter first number = ";
+    cin >> firstNumber;
+    temp1 = firstNumber;
+    cout << "Enter second number = ";
+    cin >> secondNumber;
+    temp2 = secondNumber;
+    while(firstNumber != secondNumber){
+        if (firstNumber > secondNumber)
+            firstNumber -= secondNumber;
+        else
+            secondNumber -= firstNumber;
+    }
+    cout << "Greatest common divisor = " << firstNumber << endl;
+    cout << "Least common multiple = " << (temp1 * temp2) / firstNumber << endl;
     return 0;
 }
