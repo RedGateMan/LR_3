8e27c110c50f904fe792b68d5634e04e678a2b72
RedGateMan <egor.chijov@gmail.com>
2021-09-17 20:55:49 +0300

    https://github.com/RedGateMan/LR_2

--- Tasks/Task_15/task_15.cpp
+++ Tasks/Task_15/task_15.cpp
@@ -12,6 +12,31 @@ using namespace std;
 int task_15(){
     system("chcp 65001");
     SetConsoleOutputCP(CP_UTF8);
-    cout << "Hello, World";
+    long double K;
+    cout << "Введите чило К:";
+    cin >> K;
+    long long integerAmountOfMushrooms;
+    integerAmountOfMushrooms = K;
+    if (integerAmountOfMushrooms != K){
+        cout << "мы нашли " << K << " гриба в лесу";
+    } else {
+        integerAmountOfMushrooms %= 100;
+        if(integerAmountOfMushrooms > 20) {
+            integerAmountOfMushrooms %= 10;
+        }
+
+        if (integerAmountOfMushrooms == 0) {
+            cout << "мы нашли " << K << " грибов в лесу";
+        }
+        if(integerAmountOfMushrooms == 1){
+            cout << "мы нашли " << K << " гриб в лесу";
+        }
+        if(integerAmountOfMushrooms < 5 && integerAmountOfMushrooms != 1){
+            cout << "мы нашли " << K << " гриба в лесу";
+        }
+        if(integerAmountOfMushrooms > 4){
+            cout << "мы нашли " << K << " грибов в лесу";
+        }
+    }
     return 0;
 }
