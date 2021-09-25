5845e83acf2bc78cf882c702bcdf939bf8b2817c
RedGateMan <egor.chijov@gmail.com>
2021-09-26 01:34:33 +0300

    https://github.com/RedGateMan/LR_2

--- Tasks/Task_7/task_7.cpp
+++ Tasks/Task_7/task_7.cpp
@@ -37,18 +37,43 @@ int task_7() {
                         return 0;
                     }
                 } else if (c < 0) {
-                    cout << "Solutions" << endl <<"x1 = " << sqrt(-c / b) << endl << "x2 = " << -sqrt(-c / b);
+                    cout << "Solutions" << endl << "x1 = " << sqrt(-c / b) << endl << "x2 = " << -sqrt(-c / b);
                     return 0;
                 } else {
-                    cout << "No solutions";
+                    cout << "There are no solutins for the expression";
                     return 0;
+                }
+            } else {
+                long double discriminant = pow(b, 2) - 4 * a * c;
+                if (discriminant > 0) {
+                    long double t1 = (-b - sqrt(discriminant)) / (2 * a);
+                    long double t2 = (-b + sqrt(discriminant)) / (2 * a);
+                    if (t1 >= 0 && t2 >= 0) {
+                        cout << "Solutions" << endl << "x1 = " << sqrt(t1) << " " << "x2 = " << -sqrt(t1) << endl
+                             << "x3 = " << sqrt(t2) << " " << "x4 = " << -sqrt(t2) << endl;
+                    } else if (t1 >= 0) {
+                        cout << "Solutions" << endl << "x1 = " << sqrt(t1) << endl << "x2 = " << -sqrt(t1);
+                    } else if (t2 >= 0) {
+                        cout << "Solutions" << endl << "x1 = " << sqrt(t2) << endl << "x2 = " << -sqrt(t2);
+                    } else {
+                        cout << "There are no solutions for the expression";
+                    }
+                } else if (discriminant == 0) {
+                    long double t = -b / (2 * a);
+                    if (t >= 0) {
+                        cout << "Solutions" << endl << "x1 = " << sqrt(t) << " " << "x2 = " << -sqrt(t);
+                    } else {
+                        cout << "There are no solutions for the expression";
+                    }
+                } else {
+                    cout << "There are no solutions for the expression";
                 }
             }
             break;
         }
         case 2: {
-            cout << "You've chosen 2-nf type of expression ax^4 + bx^3 + cx^2 + bx + a = 0 " << endl;
-            long double a, b, c;
+            double a, b, c;
+            cout << "You've chosen 1-st type of expression ax^4 + bx^2 + c = 0 " << endl;
             cout << "Enter coefficients: " << endl;
             cout << "a = ";
             cin >> a;
@@ -56,37 +81,16 @@ int task_7() {
             cin >> b;
             cout << "c = ";
             cin >> c;
-            long double D = pow(b, 2) - 4 * a * (c - 2 * a);
-            bool flag1, flag2;
-            long double t1, t2;
-            t1 = (-b - sqrt(D)) / (2 * a);
-            t2 = (-b + sqrt(D)) / (2 * a);
-            if (D < 0) {
-                cout << "There are no solutions for the expression :(";
-                return 1;
-            } else {
-                long double D1 = pow(t1, 2) - 4 * a * (c - 2 * a);
-                if (D1 < 0) {
-                    cout << "There are no solutions for the expression 1 :(" << endl;
-                    flag1 = false;
-                } else {
-                    long double x1 = (t1 - sqrt(pow(t1, 2) - 4)) / 2;
-                    long double x2 = (t1 + sqrt(pow(t1, 2) - 4)) / 2;
-                    cout << "x1 = " << x1 << " " << "x2 = " << x2 << endl;
-                    flag1 = true;
-                }
-                long double D2 = pow(t2, 2) - 4 * a * (c - 2 * a);
-                if (D2 < 0) {
-                    cout << "There are no solutions for the expression 2 :(" << endl;
-                    flag2 = false;
-                } else {
-                    long double x3 = (t2 - sqrt(pow(t2, 2) - 4)) / 2;
-                    long double x4 = (t2 + sqrt(pow(t2, 2) - 4)) / 2;
-                    cout << "x3 = " << x3 << " " << "x4 = " << x4 << endl;
-                    flag2 = true;
-                }
-                if (flag1 == 0 && flag2 == 0) {
-                    cout << "No solutions";
+            if(a == 0){
+                if(b == 0){
+                    if (c == 0) {
+                        //A = 0, B = 0, C = 0
+                        cout << "x belongs R";
+                        return 0;
+                    } else {
+                        cout << "No solutions";
+                        return 0;
+                    }
                 }
             }
             break;
@@ -98,5 +102,6 @@ int task_7() {
             cout << "Vadim, choose correct type of expression!";
             break;
     }
+
     return 0;
 }
