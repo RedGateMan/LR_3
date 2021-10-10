1e7520f1d3b7c257651b155292044177390a2a34
RedGateMan <egor.chijov@gmail.com>
2021-10-10 15:47:57 +0300

    https://github.com/RedGateMan/LR_3

--- Tasks/Task_9/task_9.cpp
+++ Tasks/Task_9/task_9.cpp
@@ -1,15 +1,27 @@
 //Лабораторная 3, задача 9. Выполнена: Чижов Е.С.
 
 /*
- * Вычислить
- * Входные данные таковы, что результат может быть вычислен бе переполнения.
- * Использование вешественно арифметики запрещеною
+Вычислить сумму
 */
 #include <iostream>
+#include <cmath>
+#include <ctime>
 
 using namespace std;
 
 int task_9() {
-    
+    long long N;
+    cin >> N;
+    long double a[N];
+    long double sum = 0;
+    for(int i = 0; i < N; i++){
+        //srand(time(nullptr));
+        a[i] = rand() % 1000;
+        sum += pow(2,i) * pow(-1,i) * a[i];
+    }
+    for(int i = 0; i < N; i++){
+        cout << a[i] << endl;
+    }
+    cout << sum;
     return 0;
 }
